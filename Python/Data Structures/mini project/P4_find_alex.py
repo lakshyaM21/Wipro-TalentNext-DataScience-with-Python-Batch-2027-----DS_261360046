f082fd0a82a8686b44c3e73be7e6b0c957a6422c
lakshyaM21 <lakshya21.spam@gmail.com>
2026-06-29 14:24:06 +0530

    Create P4_find_alex.py

---
+++ Python/Data Structures/mini project/P4_find_alex.py
@@ -0,0 +1,6 @@
+the_big_long_sentence = input("Sample input: ")
+the_name_to_look_out_for = "Alex"
+
+how_many_times_it_showed_up = the_big_long_sentence.count(the_name_to_look_out_for)
+
+print(how_many_times_it_showed_up)
