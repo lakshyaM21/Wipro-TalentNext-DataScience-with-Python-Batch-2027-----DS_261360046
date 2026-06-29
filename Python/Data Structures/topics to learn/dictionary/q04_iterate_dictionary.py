9e85b07e182af6bc5332672d1520f2959dd661a2
lakshyaM21 <lakshya21.spam@gmail.com>
2026-06-29 14:20:32 +0530

    Add files via upload

---
+++ Python/Data Structures/topics to learn/dictionary/q04_iterate_dictionary.py
@@ -0,0 +1,13 @@
+d={1:"Apple",2:"Banana",3:"Mango"}
+
+print("Keys:")
+for k in d:
+    print(k)
+
+print("Values:")
+for v in d.values():
+    print(v)
+
+print("Keys and Values:")
+for k,v in d.items():
+    print(k,":",v)
---
+++ Python/Data Structures/topics to learn/dictionary/q06_sum_dictionary_values.py
@@ -0,0 +1,5 @@
+d={1:10,2:20,3:30,4:40}
+
+total=sum(d.values())
+
+print("Sum =",total)
