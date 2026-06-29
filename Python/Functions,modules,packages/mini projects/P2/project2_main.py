b373ae433b5380a60d0c23597c66ba6170a51881
lakshyaM21 <lakshya21.spam@gmail.com>
2026-06-29 14:38:10 +0530

    Create project2_main.py

---
+++ Python/Functions,modules,packages/mini projects/P2/project2_main.py
@@ -0,0 +1,12 @@
+import name_analyzer_module
+
+the_name_to_investigate = input("Sample input 1: ")
+print()
+
+name_analyzer_module.ispalindrome(the_name_to_investigate)
+print()
+
+name_analyzer_module.count_the_vowels(the_name_to_investigate)
+print()
+
+name_analyzer_module.frequency_of_letters(the_name_to_investigate)
