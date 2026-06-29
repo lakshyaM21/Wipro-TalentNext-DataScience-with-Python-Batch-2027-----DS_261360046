22b3ea25104cf2761c536852d214cca5a6db12d6
lakshyaM21 <lakshya21.spam@gmail.com>
2026-06-29 14:04:58 +0530

    Create q01_remove_item.py

---
+++ Python/Data Structures/topics to learn/Set/q01_remove_item.py
@@ -0,0 +1,8 @@
+my_collection_of_shapes = {"circle", "square", "triangle", "hexagon"}
+print("Initial shapes:", my_collection_of_shapes)
+
+shape_to_get_rid_of = "square"
+my_collection_of_shapes.remove(shape_to_get_rid_of)
+
+print("Shapes after removal:", my_collection_of_shapes)
+
