0f04e4307f5da66280719d8106da36b8628a0666
lakshyaM21 <lakshya21.spam@gmail.com>
2026-06-29 14:05:24 +0530

    Create q02_set_intersection.py

---
+++ Python/Data Structures/topics to learn/Set/q02_set_intersection.py
@@ -0,0 +1,6 @@
+group_a_hobbies = {"reading", "swimming", "coding"}
+group_b_hobbies = {"coding", "painting", "reading"}
+
+hobbies_in_common = group_a_hobbies.intersection(group_b_hobbies)
+
+print("The hobbies both groups share are:", hobbies_in_common)
