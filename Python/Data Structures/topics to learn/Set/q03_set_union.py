a127e402f88dea574f426595708958b58a55cfc1
lakshyaM21 <lakshya21.spam@gmail.com>
2026-06-29 14:05:43 +0530

    Create q03_set_union.py

---
+++ Python/Data Structures/topics to learn/Set/q03_set_union.py
@@ -0,0 +1,6 @@
+morning_snacks = {"apple", "banana", "yogurt"}
+afternoon_snacks = {"pretzel", "apple", "cheese"}
+
+all_unique_snacks = morning_snacks.union(afternoon_snacks)
+
+print("Every single unique snack we had today:", all_unique_snacks)
