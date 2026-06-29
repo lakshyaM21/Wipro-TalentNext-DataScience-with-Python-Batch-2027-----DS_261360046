279b7a0366aed93e6cbb9510e038fe7956db1214
lakshyaM21 <lakshya21.spam@gmail.com>
2026-06-29 14:06:05 +0530

    Create q04_max_min_value.py

---
+++ Python/Data Structures/topics to learn/Set/q04_max_min_value.py
@@ -0,0 +1,7 @@
+daily_temperatures = {72, 65, 80, 75, 68, 82}
+
+hottest_temperature = max(daily_temperatures)
+coldest_temperature = min(daily_temperatures)
+
+print(f"The highest temperature recorded was {hottest_temperature} degrees.")
+print(f"The lowest temperature recorded was {coldest_temperature} degrees.")
